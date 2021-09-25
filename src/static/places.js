be5213ea13d260308ae47c32618062b1a790b2e4
prime-programmer <chibike.obin@gmail.com>
2021-09-25 17:54:48 +0100

    initial commit

---
+++ src/static/places.js
@@ -0,0 +1,18 @@
+const places = [
+    {
+      title: 'Maldives',
+      description:
+        "The Maldives are home to some of the world's most ravishing islands, but it's the sea, which truly makes these islands shine. Luminous aquamarine waters with a crystal clarity lap upon these dazzling white shores, which barely peek above the Indian Ocean.",
+      imageUrl: process.env.PUBLIC_URL + '/assets/smooth.jpg',
+      time: 1500,
+    },
+    {
+      title: 'Bali Islands',
+      description:
+        'Shaped like a giant sombrero, this lush volcanic island stars in countless South Pacific fantasies. The focal point and best asset of this tropical beauty is its ravishing lagoon in technicolor turquoise. Fish, turtles, sharks, and rays swim in the clear waters.',
+      imageUrl: process.env.PUBLIC_URL + '/assets/destination.jpg',
+      time: 1500,
+    },
+  ];
+  
+  export default places;
